bd770e09673f1248c5a887dd32a1c5208c57e83b
VaishnaviHagawane <91976034+VaishnaviHagawane@users.noreply.github.com>
2021-10-06 12:29:46 +0530

    Create PerfectNum.java
    Write a program to print all perfect numbers between 1 to n

---
+++ PerfectNum.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String args[]){
+         Scanner sc=new Scanner (System.in);
+         System.out.println("Enter the max range of Perfect no.");
+         int num=sc.nextInt();
+         
+            for(int i=1;i<=num;i++)
+            { 
+                int sum=0;
+                for(int div=1;div<i;div++)
+                {
+                    if(i%div==0)
+                    {
+                        sum=sum+div;
+                    }
+                }
+                if(sum==i)
+                {
+                  System.out.println(i+" is perfect number");
+                } 
+            }   
+    }
+}
