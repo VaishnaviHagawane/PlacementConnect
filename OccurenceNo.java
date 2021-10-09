daa38df47f0efecb123cceed2d53c2a308bc145e
VaishnaviHagawane <91976034+VaishnaviHagawane@users.noreply.github.com>
2021-10-09 09:56:05 +0530

    Create OccurenceNo.java
    Find total occurrences of each digit using function

---
+++ OccurenceNo.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class OccurenceNo
+{
+    public static void freqCount(String num) {
+        while(num.length()>0){
+            char n=num.charAt(0);
+            System.out.println("Frequncy of "+n+" is "+Occurence(num,n));
+            num=num.replace(""+n,"");
+            
+        }
+    }
+    public static int Occurence(String num,char n) {
+        int count=0;
+        for (int i=0;i<num.length();i++) {
+            if(num.charAt(i)==n){
+                count++;
+            }
+        }
+        return count;
+    }
+    
+    
+    
+    
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    System.out.println("Enter the number: ");
+	    String number=sc.nextLine();
+	    Main m=new Main();
+	    m.freqCount(number);
+	}
+}
